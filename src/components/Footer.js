094cf109d956278cefd9ea376117fbf7e339996f
Caryl Tan <tan.caryl@gmail.com>
2023-03-23 15:10:31 -0400

    footer stling and responsive adjusted

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -30,50 +30,52 @@ const Footer = () => {
                         </div>
 
                         <div class="col-12 col-sm-8">
-                            <div class="row">
-                                <div class="col-4 col-sm-3">
+                            <div class="row justify-content-end">
+                                <div class="col-4 col-sm-4">
                                     <ul class="list-unstyled">
                                         <li class="pb-4"><strong>Links</strong></li>
-                                        <li class="pb-2">Pricing</li>
-                                        <li class="pb-2">About Us</li>
-                                        <li class="pb-2">Careers</li>
-                                        <li class="pb-2">Blog</li>
+                                        <li class="pb-2 text-muted">Pricing</li>
+                                        <li class="pb-2 text-muted">About Us</li>
+                                        <li class="pb-2 text-muted">Careers</li>
+                                        <li class="pb-2 text-muted">Blog</li>
                                     </ul>
                                 </div>
-                                <div class="col-4 col-sm-3">
+                                <div class="col-4 col-sm-4">
                                     <ul class="list-unstyled">
                                         <li class="pb-4"><strong>Products</strong></li>
-                                        <li class="pb-2">Take The Tour</li>
-                                        <li class="pb-2">Live Chat</li>
-                                        <li class="pb-2">Self Service</li>
-                                        <li class="pb-2">Mobile</li>
-                                        <li class="pb-2">Collaboration</li>
-                                        <li class="pb-2">Reviews</li>
+                                        <li class="pb-2 text-muted">Take The Tour</li>
+                                        <li class="pb-2 text-muted">Live Chat</li>
+                                        <li class="pb-2 text-muted">Self Service</li>
+                                        <li class="pb-2 text-muted">Mobile</li>
+                                        <li class="pb-2 text-muted">Collaboration</li>
+                                        <li class="pb-2 text-muted">Reviews</li>
                                     </ul>
                                 </div>
-                                <div class="col-4 col-sm-3">
+                                <div class="col-4 col-sm-4">
                                     <ul class="list-unstyled">
                                         <li class="pb-4"><strong>Legal</strong></li>
-                                        <li class="pb-2">Terms of Use</li>
-                                        <li class="pb-2">Terms & Conditions</li>
-                                        <li class="pb-2">Privacy</li>
-                                        <li class="pb-2">Cookie Policy</li>
+                                        <li class="pb-2 text-muted">Terms of Use</li>
+                                        <li class="pb-2 text-muted">Terms & Conditions</li>
+                                        <li class="pb-2 text-muted">Privacy</li>
+                                        <li class="pb-2 text-muted">Cookie Policy</li>
                                     </ul>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="d-flex flex-row justify-content-between py-3">
-                        <div class="col-4 d-flex flex-row">
+                    <div class="d-flex py-3 flex-sm-column flex-md-row flex-wrap">
+                        <div class="col-12 d-flex flex-row col-md-4 ">
                             <p>Privacy & Terms</p>
                             <p class="px-4">Contact Us</p>
                         </div>
-                        <div class="col-4 row">
-                            <p class="text-center">Copyright ©2023 Jobi Inc.</p>
+                        <div class="col-12 flex-lg-row col-md-4 flex-sm-column">
+                            <div class="d-flex justify-content-lg-center justify-content-sm-start">
+                            <p class="text-muted">Copyright ©2023 Jobi Inc.</p>
+                            </div>
                         </div>
-                        <div class="row col-4">
-                            <ul class="list-unstyled d-flex flex-row justify-content-end">
-                                <li >
+                        <div class="row col-12 col-md-4">
+                            <ul class="list-unstyled d-flex flex-row justify-content-lg-end justify-content-sm-start">
+                                <li>
                                     <img src={Facebook} alt="Facebook social icon" class="social-icon-bottom" />
                                 </li>
                                 <li class="">
--- src/components/JobPortalSection.js
+++ src/components/JobPortalSection.js
@@ -5,16 +5,15 @@ const JobPortalSection = () => {
                 <div class="wrapper">
                     <hr />
                     <div class="row">
-                        <div class="col-sm-8 d-flex flex-column py-4">
+                        <div class="col-12 col-md-8 d-flex flex-column py-4">
                             <h2>Most complete job portal</h2>
                             <p class="text-muted">Sign up and start your search</p>
                         </div>
-                        <div class="d-flex col-4 align-items-center">
+                        <div class="d-flex col-12 col-md-4 align-items-center justify-content-end">
                             <div class="d-flex justify-content-end">
-                                <button type="button" class="btn btn-primary mx-1">Looking for a job?</button>
-                                <button type="button" class="btn btn-primary mx-1">Post a job</button>
+                                <button type="button" class="btn btn-primary mx-2">Looking for a job?</button>
+                                <button type="button" class="btn btn-primary mx-2">Post a job</button>
                             </div>
-
                         </div>
                     </div>
                     <hr />
