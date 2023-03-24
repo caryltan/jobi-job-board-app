f3259bc54590c0044448c5f815fc6fc3e7992180
Caryl Tan <tan.caryl@gmail.com>
2023-03-24 11:40:50 -0400

    responsive polish on how work section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./sass/partials/main.css";
 import { Link, Routes, Route } from 'react-router-dom';
-import Home from "./pages/home.js";
+import Home from "./pages/Home.js";
 import Footer from "./components/Footer.js"
 import Header from "./components/Header.js"
 import bootstrap from 'bootstrap'
