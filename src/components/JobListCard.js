cafe192dbcca112c2894a7100a9d50f654bda733
Caryl Tan <tan.caryl@gmail.com>
2023-03-24 19:02:31 -0400

    reusable component for job listing card populated

--- src/components/JobListCard.js
+++ src/components/JobListCard.js
@@ -1,7 +1,31 @@
 
-const JobListCard = () => {
+const JobListCard = (cardInfo) => {
+    console.log(cardInfo.cardInfo.jobListing)
     return (
-        <></>
+        <>
+            <div className="col-12 col-sm-6">
+                <div className="card border-light mx-2 my-2">
+                    <div className="card-body">
+                        <div className="card-text">
+                            <h3>{cardInfo.cardInfo.jobListing.company}</h3>
+                            {/* <img src={Rating} alt="Rating Star 5/5" /> */}
+                            <p>"This is absolutely unique and clean and what I'm looking for. Also, the code quality is great!"</p>
+                        </div>
+                        <div className="card-footer bg-transparent">
+                            <div className="d-flex flex-row">
+                                <div className="pe-3">
+                                    {/* <img src={ProfileImg} alt="Headshot of user profile" /> */}
+                                </div>
+                                <div>
+                                    <h4>Zubayer Al Hasan</h4>
+                                    <p className="text-muted">Canada</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/pages/JobList.js
+++ src/pages/JobList.js
@@ -37,28 +37,7 @@ const JobList = () => {
                     console.log(jobListing)
                     return (
                         <>
-                            <div className="col-12 col-sm-6">
-                                <div className="card border-light mx-2 my-2">
-                                    <div className="card-body">
-                                        <div className="card-text">
-                                            <h3>{jobListing.jobListing.company}</h3>
-                                            {/* <img src={Rating} alt="Rating Star 5/5" /> */}
-                                            <p>"This is absolutely unique and clean and what I'm looking for. Also, the code quality is great!"</p>
-                                        </div>
-                                        <div className="card-footer bg-transparent">
-                                            <div className="d-flex flex-row">
-                                                <div className="pe-3">
-                                                    {/* <img src={ProfileImg} alt="Headshot of user profile" /> */}
-                                                </div>
-                                                <div>
-                                                    <h4>Zubayer Al Hasan</h4>
-                                                    <p className="text-muted">Canada</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <JobListCard key={index} cardInfo={jobListing}/>
                         </>
                     )
                 })
