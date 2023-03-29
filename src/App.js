4a72c82f5fe79951160add15cf3155f84020c7bf
Caryl Tan <tan.caryl@gmail.com>
2023-03-29 18:58:59 -0400

    test

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <>
 
       <Routes>
-        <Route path="/" element={ <Home /> } />
+        {/* <Route path="/" element={ <Home /> } /> */}
         <Route path="/explore" element={ <JobList /> } /> 
         <Route path="/job-listings/:jobListingID" element={ <JobPageIndv /> } />   
       </Routes>
